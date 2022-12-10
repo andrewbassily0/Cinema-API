eb1159552b89c97465f7b1ec5d41eb4eb49a3603
unknown <vitobassily@gmail.com>
2022-12-10 19:58:00 +0200

    requirments

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Movies(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    room = models.CharField(max_length=30)
+
+class Guest(models.Model):
+    name =models.CharField(max_length=30)
+    age = models.IntegerField()
+    mobile = models.CharField(max_length=40)
+    address = models.CharField(max_length=100)
+
+class Reservation(models.Model):
+    Movies =models.ForeignKey(Movies, related_name='reservation' , on_delete=models.CASCADE)
+    Guest =models.ForeignKey(Guest, related_name='reservation', on_delete=models.CASCADE)
+    date = models.DateField()
+
+    
---
+++ tickets/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from tickets.models import Guest, Movies, Reservation
+
+class MoviesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Movies
+        fields = '__all__'
+
+
+class ReservationSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+
+class GuestSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = ['pk','reservation', 'name', 'mobile']
+
